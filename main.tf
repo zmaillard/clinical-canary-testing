5fe2c3adf65720a49dc0016e2cd77a548494df4a
Zachary Maillard <zmaillard@healthwise.org>
2020-09-17 11:14:55 -0600

    Adding Front Door

--- main.tf
+++ main.tf
@@ -64,3 +64,62 @@ resource "azurerm_storage_table" "feedback" {
   name                 = "feedback"
   storage_account_name = azurerm_storage_account.tablestorage.name
 }
+
+resource "azurerm_frontdoor_custom_https_configuration" "clinical_canary_custom_https" {
+  frontend_endpoint_id  = azurerm_frontdoor.canary_frontdoor.frontend_endpoint[0].id
+  custom_https_provisioning_enabled = false
+}
+resource "azurerm_frontdoor" "canary_frontdoor" {
+  name                                         = "Clinical-Canary-FrontDoor"
+  resource_group_name                          = azurerm_resource_group.rg.name
+  enforce_backend_pools_certificate_name_check = false
+
+  routing_rule {
+    name               = "Clinical-Canary-FrontDoor-Routing"
+    accepted_protocols = ["Http", "Https"]
+    patterns_to_match  = ["/*"]
+    frontend_endpoints = ["Clinical-Canary-FrontEnd"]
+    forwarding_configuration {
+      forwarding_protocol = "MatchRequest"
+      backend_pool_name   = "CanaryStorageBackend"
+    }
+  }
+
+  backend_pool_load_balancing {
+    name = "clin-canary-load-balancing"
+  }
+
+  backend_pool_health_probe {
+    name = "clin-canary-health-probe"
+  }
+
+  backend_pool {
+    name = "CanaryStorageBackend"
+    backend {
+      host_header = "clincanaryblue.blob.core.windows.net"
+      address     = "clincanaryblue.blob.core.windows.net"
+      http_port   = 80
+      https_port  = 443
+      priority    = 1
+      weight      = 1000
+    }
+
+    backend {
+      host_header = "clincanarygreen.blob.core.windows.net"
+      address     = "clincanarygreen.blob.core.windows.net"
+      http_port   = 80
+      https_port  = 443
+      priority    = 1
+      weight      = 1000
+    }
+
+    load_balancing_name = "clin-canary-load-balancing"
+    health_probe_name   = "clin-canary-health-probe"
+  }
+
+  frontend_endpoint {
+    name                              = "Clinical-Canary-FrontEnd"
+    host_name                         = "clin-canary-test.azurefd.net"
+    session_affinity_enabled          = true
+  }
+}
